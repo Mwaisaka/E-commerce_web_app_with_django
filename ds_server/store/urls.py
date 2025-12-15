c5591af3994c8d4178429812d61701b7e1d22062
Frank Mwaisaka <frankkashere@gmail.com>
2025-12-15 13:00:38 +0300

    Add : Added Home page and Login Views/Pages

--- ds_server/store/urls.py
+++ ds_server/store/urls.py
@@ -1,6 +1,12 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from .views.home import Index, store
+from .views.login import Login, logout
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', store, name='homepage'),
+    # path('store', store, name='store'),
+    path('cart/', Index.as_view(), name='cart'),  # cart actions
+    path('login/', Login.as_view(), name='login'),
+    path('logout/', logout, name='logout'),
 ]
